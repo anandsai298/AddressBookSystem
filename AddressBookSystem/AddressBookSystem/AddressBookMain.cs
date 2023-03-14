55325eb9096765f73e3533cfe8cfef291744799f
anandsaikumar <anandsai298@gmail.com>
2023-03-14 21:46:28 +0530

    [sai] Add : created delete contact an dedit it nby using firstname equals name

--- AddressBookSystem/AddressBookSystem/AddressBookMain.cs
+++ AddressBookSystem/AddressBookSystem/AddressBookMain.cs
@@ -8,9 +8,10 @@ namespace AddressBookSystem
 {
     public class AddressBookMain
     {
-        Contact contact = new Contact();//global declaration
+        List<Contact> addressbook = new List<Contact>();
         public void AddContact()
         {
+            Contact contact = new Contact();//local
             Console.WriteLine("Enter First Name ");
             contact.Fn = Console.ReadLine();
             Console.WriteLine("Enter Last Name ");
@@ -27,42 +28,67 @@ namespace AddressBookSystem
             contact.PhNo = Console.ReadLine();
             Console.WriteLine("Enter Email Id ");
             contact.Email = Console.ReadLine();
+            addressbook.Add(contact);
         }
         public void Display()
         {
+            foreach(var contact in addressbook) 
+            { 
             Console.WriteLine("First Name--->"+contact.Fn + "\n" + "Last Name--->"+ contact.Ln + "\n" +"Address--->"+ contact.Address + "\n" +"State--->"+ contact.State + "\n" +"City--->"+ contact.City + "\n" +"ZipCode--->"+ contact.ZipCode + "\n" + "Phone Number--->"+contact.PhNo + "\n" +"Email Id--->"+ contact.Email);
+            }
         }
         public void Edit_Contact()
         {
-            Console.WriteLine("Enter option to edit \n1.First Name\n2.Last Name\n3.Address\n4.State\n5.City\n6.ZipCode\n7.Phone Number\n8.Email Id");
-            int op = Convert.ToInt32(Console.ReadLine());
-            switch(op)
+            Console.WriteLine("Edit using First Name");
+            string name = Console.ReadLine();
+            foreach(var data in addressbook) 
             {
-                case 1:
-                    contact.Fn = Console.ReadLine();
-                    break;
-                case 2:
-                    contact.Ln = Console.ReadLine();
-                    break;
-                case 3:
-                    contact.Address = Console.ReadLine();
-                    break;
-                case 4:
-                    contact.State = Console.ReadLine();
-                    break;
-                case 5:
-                    contact.City = Console.ReadLine();
-                    break;
-                case 6:
-                    contact.ZipCode = Console.ReadLine();
-                    break;
-                case 7:
-                    contact.PhNo = Console.ReadLine();
-                    break;
-                case 8:
-                    contact.Email = Console.ReadLine();
-                    break;
+                if (data.Fn == name) 
+                { 
+                    Console.WriteLine("Enter option to edit \n1.First Name\n2.Last Name\n3.Address\n4.State\n5.City\n6.ZipCode\n7.Phone Number\n8.Email Id");
+                    int op = Convert.ToInt32(Console.ReadLine());
+                    switch(op)
+                    {
+                        case 1:
+                            data.Fn = Console.ReadLine();
+                            break;
+                        case 2:
+                            data.Ln = Console.ReadLine();
+                            break;
+                        case 3:
+                            data.Address = Console.ReadLine();
+                            break;
+                        case 4:
+                            data.State = Console.ReadLine();
+                            break;
+                        case 5:
+                            data.City = Console.ReadLine();
+                            break;
+                        case 6:
+                            data.ZipCode = Console.ReadLine();
+                            break;
+                        case 7:
+                            data.PhNo = Console.ReadLine();
+                            break;
+                        case 8:
+                            data.Email = Console.ReadLine();
+                            break;
+                    }
+                }
             }
+        }
+        public void Delete_Contact()
+        {
+            Console.WriteLine("Enter name to delete contact");
+            string name = Console.ReadLine();
+            foreach (var data in addressbook)
+            {
+                if(data.Fn.Equals(name))
+                {
+                    addressbook.Remove(data);
+                }
+            }
+
         }
     }
 }
--- AddressBookSystem/AddressBookSystem/Program.cs
+++ AddressBookSystem/AddressBookSystem/Program.cs
@@ -9,18 +9,25 @@ namespace AddressBookSystem
             bool flag = true;
             while(flag)
             {
-                Console.WriteLine("1.Add_Contact\n2.EditContact");
+                Console.WriteLine("1.Add_Contact\n2.EditContact\n3.DeleteContact");
                 Console.WriteLine("Enter option to display option");
                 int option = Convert.ToInt32(Console.ReadLine());
                 AddressBookMain ABM = new AddressBookMain();
                 switch (option)
                 {
                     case 1:
+                        for (int i = 0; i < 2; i++) 
+                        {
                         ABM.AddContact();
+                        }
                         ABM.Display();
                         break;
                     case 2:
                         ABM.Edit_Contact();
+                        ABM.Display();
+                        break;
+                    case 3:
+                        ABM.Delete_Contact();
                         ABM.Display();
                         break;
                 }
