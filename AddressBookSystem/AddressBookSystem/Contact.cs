b8be1a704a6484ccbed3ed8736bff3b81837c5da
anandsaikumar <anandsai298@gmail.com>
2023-03-14 18:37:41 +0530

    [sai] Add : created contacts

--- AddressBookSystem/AddressBookSystem/Contact.cs
+++ AddressBookSystem/AddressBookSystem/Contact.cs
@@ -8,6 +8,13 @@ namespace AddressBookSystem
 {
     public class Contact
     {
-        
+        public String Fn { get; set; }
+        public String Ln { get; set; }
+        public String Address { get; set; }
+        public String State { get; set; }
+        public String City { get; set; }
+        public String ZipCode { get; set; }
+        public String PhNo { get; set; }
+        public String Email { get; set; }
     }
 }
