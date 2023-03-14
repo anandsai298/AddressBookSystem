463a4ac69d7b49b4e953ac48d7e2dac4a1e36df0
anandsaikumar <anandsai298@gmail.com>
2023-03-14 18:27:26 +0530

    [sai] : contacts are created like name address phno email...

---
+++ AddressBookSystem/AddressBookSystem/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace AddressBookSystem
+{
+    class program
+    {
+        static void Main(String[] args)
+        {
+            Contact contact = new Contact();
+            Console.WriteLine("Enter First Name ");
+            contact.Fn = Console.ReadLine();
+            Console.WriteLine("Enter Last Name ");
+            contact.Ln = Console.ReadLine();
+            Console.WriteLine("Enter Address ");
+            contact.Address = Console.ReadLine();
+            Console.WriteLine("Enter State ");
+            contact.State = Console.ReadLine();
+            Console.WriteLine("Enter City ");
+            contact.City = Console.ReadLine();
+            Console.WriteLine("Enter ZipCode ");
+            contact.ZipCode = Console.ReadLine();
+            Console.WriteLine("Enter Phone Number ");
+            contact.PhNo = Console.ReadLine();
+            Console.WriteLine("Enter Email Id ");
+            contact.Email = Console.ReadLine();
+        }
+    }
+}
